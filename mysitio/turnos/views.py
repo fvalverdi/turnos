0530f2933f9c913913739921630c76f9f7ac2a32
facundo <facundovalverdi.fv@gmail.com>
2016-04-08 11:27:58 -0300

    Por Armar el Listado de Pacientes

--- mysitio/turnos/views.py
+++ mysitio/turnos/views.py
@@ -1,3 +1,8 @@
+#from django.http import HttpResponse
 from django.shortcuts import render
+#from .models import Turnos
+
+def pacientes_list(request):
+        return render(request, 'turnos/pacientes_list.html', {})
 
 # Create your views here.
